0802c64d16954d552b20b05ac7fe9ffd401dba09
Thevindu_Senanayake <thevindusenanayake76@gmail.com>
2022-07-28 15:48:49 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState(["hehe", "shanoo"]);
+	const [input, setInput] = useState("");
+
+	const addTodo = (e) => {
+		e.preventDefault();
+		setTodos([...todos, input]);
+		setInput("");
+	};
+
+	return (
+		<div className="App">
+			<h1 className="hedder">Hello</h1>
+			<form>
+				<input value={input} onChange={(e) => setInput(e.target.value)} />
+				<button onClick={addTodo}>Add Todo</button>
+			</form>
+
+			<ul>
+				{todos.map((todo) => (
+					<li>{todo}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
